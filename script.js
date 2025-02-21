c08b29fe6e20b0acb614e13f2489ae318465f24e
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-21 10:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+// Create audio elements and append them to the DOM
+sounds.forEach(sound => {
+    const audio = document.createElement('audio');
+    audio.id = sound;
+    audio.src = `sounds/${sound}.mp3`;
+    document.body.appendChild(audio);
+});
+
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', () => {
         const sound = button.getAttribute('data-sound');
-        const audio = new Audio(`sounds/${sound}.mp3`);
+        const audio = document.getElementById(sound);
+        audio.currentTime = 0; // Rewind to the start
         audio.play();
     });
 });
@@ -12,3 +23,4 @@ document.querySelector('.stop').addEventListener('click', () => {
         audio.currentTime = 0;
     });
 });
+
